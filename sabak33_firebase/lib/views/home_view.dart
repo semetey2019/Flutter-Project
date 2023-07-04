1bf67e73720c815d9d84e4e486f79d8bdb70e5ec
Tolgonai <tolgonai.mamytova.bj@gmail.com>
2023-07-04 13:21:13 +0300

    done

--- sabak33_firebase/lib/views/home_view.dart
+++ sabak33_firebase/lib/views/home_view.dart
@@ -28,77 +28,80 @@ class _HomeViewState extends State<HomeView> {
   Future<void> deleteTodo(Todo todo) async {
     final db = FirebaseFirestore.instance;
     await db.collection('todos').doc(todo.id).delete();
+  }
 
-    @override
-    void initState() {
-      readTodo();
-      super.initState();
-    }
+  @override
+  void initState() {
+    readTodo();
+    super.initState();
+  }
 
-    @override
-    Widget build(BuildContext context) {
-      return Scaffold(
-        appBar: AppBar(
-          centerTitle: true,
-          title: const Text('HomeView'),
-        ),
-        body: StreamBuilder(
-          stream: readTodo(),
-          builder: (context, snapshot) {
-            if (snapshot.connectionState == ConnectionState.waiting) {
-              return const Center(
-                  child: CupertinoActivityIndicator(
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        centerTitle: true,
+        title: const Text('HomeView'),
+      ),
+      body: StreamBuilder(
+        stream: readTodo(),
+        builder: (context, snapshot) {
+          if (snapshot.connectionState == ConnectionState.waiting) {
+            return const Center(
+              child: CupertinoActivityIndicator(
                 radius: 20,
-              ));
-            } else if (snapshot.hasError) {
-              return Text(snapshot.error.toString());
-            } else if (snapshot.hasData) {
-              final List<Todo> todos = snapshot.data!.docs
-                  .map((e) =>
-                      Todo.fromMap(e.data() as Map<String, dynamic>)..id = e.id)
-                  .toList();
-              return ListView.builder(
-                  itemCount: todos.length,
-                  itemBuilder: (context, index) {
-                    final todo = todos[index];
-                    return Card(
-                      child: ListTile(
-                        leading: Text(todo.title),
-                        title: Text(todo.description),
-                        subtitle: Text(todo.author),
-                        trailing: Row(
-                          mainAxisSize: MainAxisSize.min,
-                          children: [
-                            Checkbox(
-                                value: todo.isCompleted,
-                                onChanged: (v) async {
-                                  await updateTodo(todo);
-                                }),
-                            IconButton(
-                              onPressed: () async {
-                                await deleteTodo(todo);
-                              },
-                              icon: const Icon(Icons.delete),
-                            ),
-                          ],
-                        ),
+              ),
+            );
+          } else if (snapshot.hasError) {
+            return Text(snapshot.error.toString());
+          } else if (snapshot.hasData) {
+            final List<Todo> todos = snapshot.data!.docs
+                .map((e) =>
+                    Todo.fromMap(e.data() as Map<String, dynamic>)..id = e.id)
+                .toList();
+            return ListView.builder(
+                itemCount: todos.length,
+                itemBuilder: (context, index) {
+                  final todo = todos[index];
+                  return Card(
+                    child: ListTile(
+                      leading: Text(todo.title),
+                      title: Text(todo.description),
+                      subtitle: Text(todo.author),
+                      trailing: Row(
+                        mainAxisSize: MainAxisSize.min,
+                        children: [
+                          Checkbox(
+                            value: todo.isCompleted,
+                            onChanged: (v) async {
+                              await updateTodo(todo);
+                            },
+                          ),
+                          IconButton(
+                            onPressed: () async {
+                              await deleteTodo(todo);
+                            },
+                            icon: const Icon(Icons.delete),
+                          ),
+                        ],
                       ),
-                    );
-                  });
-            } else {
-              return const Text("Белгисиз ката бар");
-            }
-          },
-        ),
-        floatingActionButton: FloatingActionButton(
-            backgroundColor: Colors.limeAccent,
-            onPressed: () {
-              Navigator.push(
-                context,
-                MaterialPageRoute(builder: (context) => const TodoView()),
-              );
-            }),
-      );
-    }
+                    ),
+                  );
+                });
+          } else {
+            return const Text("Белгисиз ката бар");
+          }
+        },
+      ),
+      floatingActionButton: FloatingActionButton(
+        backgroundColor: Colors.limeAccent,
+        onPressed: () {
+          Navigator.push(
+            context,
+            MaterialPageRoute(builder: (context) => const TodoView()),
+          );
+        },
+      ),
+    );
   }
 }
