8f5ec037abd7f0af97bff3a1478cb2ce772571ce
Tolgonai <tolgonai.mamytova.bj@gmail.com>
2024-04-26 16:35:44 +0300

    do

--- nippon_habbits/lib/main/main_page.dart
+++ nippon_habbits/lib/main/main_page.dart
@@ -33,81 +33,88 @@ class _MainPageState extends State<MainPage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        body: SafeArea(
-          child: PageView(
-            controller: pageController,
-            physics: const NeverScrollableScrollPhysics(),
-            onPageChanged: (value) {
-              _currentIndex = value;
-              setState(() {});
-            },
-            children: const [
-              HabitsPage(),
-              JournalPage(),
-              PracticesPage(),
-              SettingsPage(),
-            ],
-          ),
+      body: SafeArea(
+        child: PageView(
+          controller: pageController,
+          physics: const NeverScrollableScrollPhysics(),
+          onPageChanged: (value) {
+            _currentIndex = value;
+            setState(() {});
+          },
+          children: const [
+            HabitsPage(),
+            JournalPage(),
+            PracticesPage(),
+            SettingsPage(),
+          ],
         ),
-        bottomNavigationBar: Theme(
-            data: Theme.of(context).copyWith(
-              splashColor: Colors.transparent,
-              highlightColor: Colors.transparent,
+      ),
+      bottomNavigationBar: Theme(
+        data: Theme.of(context).copyWith(
+          splashColor: Colors.transparent,
+          highlightColor: Colors.transparent,
+        ),
+        child: BottomNavigationBar(
+          currentIndex: _currentIndex,
+          onTap: _onTabTapped,
+          type: BottomNavigationBarType.fixed,
+          selectedLabelStyle: const TextStyle(
+            fontSize: 11,
+            fontWeight: FontWeight.w400,
+            height: 1.6,
+          ),
+          unselectedItemColor: Colors.black,
+          unselectedLabelStyle: const TextStyle(
+            fontWeight: FontWeight.w400,
+            fontSize: 11,
+            height: 1.6,
+          ),
+          selectedItemColor: Colors.red,
+          items: [
+            BottomNavigationBarItem(
+              icon: SvgPicture.asset(
+                _currentIndex == 0
+                    ? "assets/images/habits.svg"
+                    : "assets/images/habits1.svg",
+                width: 22,
+                height: 22,
+              ),
+              label: "Habits",
             ),
-            child: BottomNavigationBar(
-                currentIndex: _currentIndex,
-                onTap: _onTabTapped,
-                type: BottomNavigationBarType.fixed,
-                selectedLabelStyle: const TextStyle(
-                  fontSize: 11,
-                  fontWeight: FontWeight.w400,
-                  height: 1.6,
-                ),
-                unselectedItemColor: Colors.black,
-                unselectedLabelStyle: const TextStyle(
-                  fontWeight: FontWeight.w400,
-                  fontSize: 11,
-                  height: 1.6,
-                ),
-                selectedItemColor: Colors.blue,
-                items: [
-                  BottomNavigationBarItem(
-                    label: "Башкы бет",
-                    icon: SvgPicture.asset(
-                      "assets/images/habits.svg",
-                      width: 22,
-                      height: 22,
-                      color: _currentIndex == 0 ? Colors.blue : Colors.black,
-                    ),
-                  ),
-                  BottomNavigationBarItem(
-                    icon: SvgPicture.asset(
-                      "assets/images/habits.svg",
-                      width: 22,
-                      height: 22,
-                      color: _currentIndex == 1 ? Colors.blue : Colors.black,
-                    ),
-                    label: "Суроо_жооп",
-                  ),
-                  BottomNavigationBarItem(
-                    icon: SvgPicture.asset(
-                      "assets/images/habits.svg",
-                      width: 22,
-                      height: 22,
-                      color: _currentIndex == 2 ? Colors.blue : Colors.black,
-                    ),
-                    label: "Куран",
-                  ),
-                  BottomNavigationBarItem(
-                    icon: SvgPicture.asset(
-                      "assets/images/habits.svg",
-                      width: 22,
-                      height: 22,
-                      color: _currentIndex == 3 ? Colors.blue : Colors.black,
-                    ),
-                    label: "Зикир",
-                  ),
-                ])));
+            BottomNavigationBarItem(
+              icon: SvgPicture.asset(
+                _currentIndex == 1
+                    ? "assets/images/journal1.svg"
+                    : "assets/images/journal.svg",
+                width: 22,
+                height: 22,
+              ),
+              label: "Journal",
+            ),
+            BottomNavigationBarItem(
+              icon: SvgPicture.asset(
+                _currentIndex == 2
+                    ? "assets/images/practices1.svg"
+                    : "assets/images/practices.svg",
+                width: 22,
+                height: 22,
+              ),
+              label: "Practices",
+            ),
+            BottomNavigationBarItem(
+              label: "Settings",
+              icon: SvgPicture.asset(
+                _currentIndex == 3
+                    ? "assets/images/settings1.svg"
+                    : "assets/images/settings.svg",
+                width: 22,
+                height: 22,
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
   }
 
   void _onTabTapped(int index) {
