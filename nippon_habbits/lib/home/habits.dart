dc30fb5b1bca7dc5deabf750ff4309fe84c528d8
Tolgonai <tolgonai.mamytova.bj@gmail.com>
2024-04-29 00:27:25 +0300

    do

--- nippon_habbits/lib/home/habits.dart
+++ nippon_habbits/lib/home/habits.dart
@@ -11,12 +11,41 @@ class _HabitsPageState extends State<HabitsPage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: const Color(0xfff5f4f9),
       appBar: AppBar(
-          centerTitle: true,
-          title: const Text(
-            "Daily habits",
-            style: TextStyle(color: Colors.red),
-          )),
+        backgroundColor: const Color(0xfffafafe),
+        surfaceTintColor: const Color(0xfffafafe),
+        automaticallyImplyLeading: false,
+        centerTitle: true,
+        elevation: 1,
+        shadowColor: Colors.black,
+        title: const Text(
+          "Daily habits",
+          style: TextStyle(color: Colors.red),
+        ),
+      ),
+      body: Stack(
+        children: [
+          Column(
+            children: [
+              DecoratedBox(
+                decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(10),
+                  color: Colors.white,
+                  boxShadow: [
+                    BoxShadow(
+                      color: const Color(0xff000000).withOpacity(0.20),
+                      spreadRadius: 0,
+                      blurRadius: 2,
+                      offset: const Offset(0, 1),
+                    ),
+                  ],
+                ),
+              ),
+            ],
+          ),
+        ],
+      ),
     );
   }
 }
